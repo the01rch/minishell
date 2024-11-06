1987ce7bcad3b4f366de93190b5ab39d26ea04a1
the01rch <reda.rouich@epitech.eu>
2024-11-06 15:38:02 +0100

    [ADD] lib.h

--- icl/minishell.h
+++ icl/minishell.h
@@ -1,34 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: redrouic <redrouic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/06 15:15:56 by redrouic          #+#    #+#             */
-/*   Updated: 2024/11/06 15:33:47 by redrouic         ###   ########.fr       */
+/*   Created: 2024/11/06 15:33:18 by redrouic          #+#    #+#             */
+/*   Updated: 2024/11/06 15:33:26 by redrouic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../icl/minishell.h"
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char *str)
-{
-	while (*str)
-		ft_putchar(*str++);
-}
-
-int	main(int ac, char **av, char **env)
-{
-	(void)ac;
-	(void)av;
-	(void)env;
-	ft_putstr("$> ");
-	while (1) {}
-	return (0);
-}
+#include <unistd.h>
